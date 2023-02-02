91f3943c3b01050e4b1a2543a35a5d0e7d60256e
kinuxroot <kinuxroot@163.com>
2023-02-02 12:01:57 +0800

    fix(json_utils): fix get bug

--- src/utils/json_utils.cpp
+++ src/utils/json_utils.cpp
@@ -4,8 +4,9 @@
 namespace utils::json {
     nlohmann::json& get(nlohmann::json& jsonObj, const std::string& key) {
         auto jsonItem = jsonObj.find(key);
-        if (jsonItem != jsonObj.end()) {
-            throw fmt::format("Json object has no entry: {}", key);
+        if (jsonItem == jsonObj.end()) {
+            std::string errorMessage = fmt::format("Json object has no entry: {}", key);
+            throw std::exception(errorMessage.c_str());
         }
 
         return jsonObj[key];
@@ -13,8 +14,9 @@ namespace utils::json {
 
     const nlohmann::json& get(const nlohmann::json& jsonObj, const std::string& key) {
         auto jsonItem = jsonObj.find(key);
-        if (jsonItem != jsonObj.end()) {
-            throw fmt::format("Json object has no entry: {}", key);
+        if (jsonItem == jsonObj.end()) {
+            std::string errorMessage = fmt::format("Json object has no entry: {}", key);
+            throw std::exception(errorMessage.c_str());
         }
 
         return jsonObj[key];
