5e6eb0aee4bdcad63c7ca602c580decf3f428566
kinuxroot <kinuxroot@163.com>
2023-02-04 22:15:31 +0800

    feat(union_find): add merge union find

---
+++ src/btc_merge_union_find/logger.cpp
@@ -0,0 +1,16 @@
+#include "btc_gen_address/logger.h"
+
+LoggerType& getLogger() {
+    using logging::LoggerFactory;
+    using logging::Level;
+    using logging::handlers::StreamHandler;
+    using logging::handlers::FileHandler;
+    using logging::formatters::cstr::formatRecord;
+
+    static auto logger = LoggerFactory<Level::Debug>::createLogger("UnionFind TxInputs", std::make_tuple(
+        StreamHandler<Level::Debug>(formatRecord),
+        FileHandler<Level::Debug>::create("logs/btc_gen_day_ins.log", formatRecord)
+    ));
+
+    return logger;
+}
