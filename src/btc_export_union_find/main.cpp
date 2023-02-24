45b9a57765d34e4b3e0659e0c2fa404dfe4260ef
kinuxroot <kinuxroot@163.com>
2023-02-24 23:14:28 +0800

    feat(export): support exclude some addresses

--- src/btc_export_union_find/main.cpp
+++ src/btc_export_union_find/main.cpp
@@ -1,6 +1,7 @@
 #include "btc-config.h"
 #include "btc_gen_day_ins/logger.h"
 
+#include "utils/io_utils.h"
 #include "utils/mem_utils.h"
 #include "utils/union_find.h"
 #include "fmt/format.h"
@@ -16,7 +17,7 @@ int main(int argc, char* argv[]) {
     using utils::btc::BtcSize;
 
     if (argc < 3) {
-        std::cerr << "Invalid arguments!\n\nUsage: btc_export_union_find <uf> <output_file>" << std::endl;
+        std::cerr << "Invalid arguments!\n\nUsage: btc_export_union_find <uf> <output_file> <exclusive_file>" << std::endl;
 
         return EXIT_FAILURE;
     }
@@ -34,16 +35,46 @@ int main(int argc, char* argv[]) {
         const char* outputFilePath = argv[2];
         std::ofstream outputFile(outputFilePath);
         logger.info(fmt::format("Dump union find result to: {}", outputFilePath));
+
+        std::set<BtcId> exchangeRootAddresseIds;
+        if (argc == 4) {
+            const char* exclusiveFilePath = argv[3];
+            std::vector<BtcId> addressIds = utils::readLines<BtcId>(
+                exclusiveFilePath,
+                [](const std::string& line) -> BtcId {
+                    return std::stoi(line);
+                }
+            );
+
+            for (BtcId addressId : addressIds) {
+                exchangeRootAddresseIds.insert(quickUnion.findRoot(addressId));
+            }
+        }
         
         const auto& quickUnionClusters = utils::btc::WeightedQuickUnionClusters(quickUnion);
         BtcSize dumpedClusterCount = 0;
-        quickUnionClusters.forEach([&outputFile, &dumpedClusterCount](BtcId btcId, BtcSize btcSize) {
+        BtcSize skippedClusterCount = 0;
+        BtcSize skippedAddressCount = 0;
+        quickUnionClusters.forEach([
+            &outputFile, &dumpedClusterCount, &exchangeRootAddresseIds, &skippedClusterCount, &skippedAddressCount
+        ](BtcId btcId, BtcSize btcSize) {
+            if (exchangeRootAddresseIds.contains(btcId)) {
+                logger.info(fmt::format("Skip btcId {} btcSize {}", btcId, btcSize));
+
+                skippedAddressCount += btcSize;
+                ++ skippedClusterCount;
+
+                return;
+            }
+
             outputFile << btcId << "," << btcSize << std::endl;
 
             ++dumpedClusterCount;
         });
 
         logger.info(fmt::format("Dump {} union find clusters", dumpedClusterCount));
+        logger.info(fmt::format("Skip {} union find clusters", skippedClusterCount));
+        logger.info(fmt::format("Skip {} addresses", skippedAddressCount));
     }
     catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
